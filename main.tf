3f0b140638818c836348991f26144fa9d0e0039e
ccitapr2024 <ccitapr2024@gmail.com>
2025-02-25 17:46:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,10 @@ resource "aws_s3_bucket" "ccit_s3new1" {
   bucket = "ccit-public-bucket2"
   tags = { Name = "ccit-public-bucket" }
 }
+resource "aws_s3_bucket" "ccit_s3new2" {
+  bucket = "ccit-public-bucket3"
+  tags = { Name = "ccit-public-bucket" }
+}
 output "s3_bucket_name" {
 description = "The name of the created S3 bucket"
 value = aws_s3_bucket.ccit_s3.id
